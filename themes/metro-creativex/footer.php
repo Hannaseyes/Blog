baf99cfae2bd2c64999e9c0290fcc8383b015e60
Hannaseyes <569797587@qq.com>
2017-09-26 15:46:47 +0800

    更新主题

--- themes/metro-creativex/footer.php
+++ themes/metro-creativex/footer.php
@@ -1,1 +1,11 @@
-		<div class="left-sidebar sidebar-mobile">			<?php get_sidebar("mobile"); ?>		</div>		<div class="clearfix"></div>		<footer>			<span class="alignleft">&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'metro-creativex'), 'http://www.wordpress.org' ); ?></span></span><br\>			<span class="alignright"><?php printf( __( 'Theme by <a rel="nofollow" target="_blank" href="%s">ThemeIsle</a>', 'metro-creativex'), 'https://themeisle.com/themes/metrox/' ); ?> </span>						</footer>	<?php wp_footer(); ?>	</body></html>
+		<div class="left-sidebar sidebar-mobile">
+			<?php get_sidebar("mobile"); ?>
+		</div>
+
+		<div class="clearfix"></div>
+		<footer>
+				<a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank">©2017 Hannaseyes | <?php echo get_option( 'zh_cn_l10n_icp_num' );?></a> </span>
+		</footer>
+	<?php wp_footer(); ?>
+	</body>
+</html>
