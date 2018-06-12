0c531af336d9396a7c1ed627c5ca495e9e3ac200
Hannaseyes <569797587@qq.com>
2018-06-12 14:48:19 +0800

    备份

--- themes/metro-creativex/footer.php
+++ themes/metro-creativex/footer.php
@@ -1,11 +1,13 @@
 		<div class="left-sidebar sidebar-mobile">
-			<?php get_sidebar("mobile"); ?>
+			<?php get_sidebar( 'mobile' ); ?>
 		</div>
 
 		<div class="clearfix"></div>
 		<footer>
-				<a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank">©2017 Hannaseyes | <?php echo get_option( 'zh_cn_l10n_icp_num' );?></a> </span>
-		</footer>
+			<span class="alignleft">&copy; <?php echo date( 'Y' ) ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a><span> &mdash; <?php printf( __( 'Powered by <a href="%s">WordPress</a>', 'metro-creativex' ), 'http://www.wordpress.org' ); ?></span></span><br\>
+			<span class="alignright"><?php printf( __( 'Theme by <a rel="nofollow" target="_blank" href="%s">ThemeIsle</a>', 'metro-creativex' ), 'https://themeisle.com/themes/metrox/' ); ?> </span>
+			
+			</footer>
 	<?php wp_footer(); ?>
 	</body>
 </html>
